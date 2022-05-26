308292351ac5d7a58a1e4552d80bf1c12646a53c
upul <upul@pop-os.localdomain>
2022-05-26 23:42:16 +0900

    ready to deploy

---
+++ R/code snippets.R
@@ -0,0 +1,14 @@
+
+# to insert download links 
+
+#| echo: false
+downloadthis::download_link(
+  link = "https://github.com/upulcooray/Stat-ClubR/blob/main/resources/upul.jpg",
+  button_label = "Download pdf file",
+  button_type = "default",
+  has_icon = TRUE,
+  icon = "fa fa-save",
+  self_contained = FALSE,
+  class= "hvr-sweep-to-left"
+)
+
